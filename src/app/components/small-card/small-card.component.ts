6a97aa444f2ddcaca800ca77793de6553b83158e
Diego <carvalho.84@gmail.com>
2023-10-17 10:23:48 -0300

    components dynamic propertys

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrls: ['./small-card.component.css']
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit{
+
+  @Input()
+  photoCover: string = "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus.jpg"
+
+  @Input()
+  cardTitle: string = "Nova série anunciada"
+
+  constructor() { }
+
+  ngOnInit(): void {}
 
 }
